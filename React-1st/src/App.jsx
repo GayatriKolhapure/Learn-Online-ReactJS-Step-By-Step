b2f5939f86786b94c5922ff5ef4ee154e5f429fb
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2026-06-11 16:59:05 +0530

    Added header file first component

--- React-1st/src/App.jsx
+++ React-1st/src/App.jsx
@@ -1,14 +1,21 @@
-
-import './App.css'
-
+import "./App.css";
+import Header from "./Header";
 function App() {
-
   return (
     <>
-    <h2>Code Step by Step</h2>
-    <h3>Hello React</h3>
+      <h2>Code Step by Step</h2>
+      <h3>Hello React</h3>
+      <Header/>
+      <h1>Code flow to run react js project</h1>
+      <ul>
+        <h3>
+          <li>index.html</li>
+          <li>main.jsx</li>
+          <li>App.jsx</li>
+        </h3>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React-1st/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <>
+        <h4>Header File from Header component</h4>
+        </>
+
+    )
+}
+export default Header;
