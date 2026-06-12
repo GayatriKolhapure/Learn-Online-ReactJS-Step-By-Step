3e30bb575ab98102be29283cda279d2c861a9d6e
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2026-06-12 11:19:14 +0530

    first task completed

---
+++ React-2nd/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Tommy</h1>
+    <img style={{height:'300px', width:'300px'}}  src='https://m.media-amazon.com/images/I/519Hct2QoeL._AC_UF894,1000_QL80_.jpg'></img>
+    <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehase a movie scene</li>
+      <li>Improve the spectrum technology</li>
+    </ul>
+    <button onClick={() => alert("function called")}>Click me</button>
+    </>
+  )
+}
+
+export default App
