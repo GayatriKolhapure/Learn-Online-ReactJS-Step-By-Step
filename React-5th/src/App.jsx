f5119bb0803680e3fb433d4274366cc4a0f02860
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2026-06-18 23:25:09 +0530

    started useRef 1st part done

---
+++ React-5th/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UseRef from './UseRef1st'
+
+function App() {
+
+
+  return (
+    <>
+     <UseRef />
+    </>
+  )
+}
+
+export default App
---
+++ React-5th/src/UseRef1st.jsx
@@ -0,0 +1,47 @@
+import { useRef } from "react"
+
+const UseRef = () =>{
+
+    const inputRef = useRef(null);
+    const h1Ref = useRef(null);
+
+    const inputHandler = () =>{
+        console.log(inputRef);
+        inputRef.current.focus();
+        inputRef.current.style.color="red";
+        inputRef.current.placeholder="Enter a password";
+        inputRef.current.value="123"
+        
+    }
+
+    const toggleHandler=()=>{
+        if(inputRef.current.style.display != 'none'){
+            inputRef.current.style.display='none';
+        }else{
+             inputRef.current.style.display='inline';
+        }
+    }
+    const displayH1=()=>{
+        if(h1Ref.current.style.color != 'green'){
+            h1Ref.current.style.color='green';
+        }else{
+             h1Ref.current.style.color='white';
+        }
+    }
+
+
+
+    return(
+        <>
+        <h2>useref in React</h2>
+        <button onClick={toggleHandler}>Toggle</button>
+        <input ref={inputRef} type="text" placeholder="enter your name"/>
+        <button onClick={inputHandler}>Focus on input field</button>
+        <hr/>
+        <h1 ref={h1Ref}>Learn Code with me!</h1>
+        <button onClick={displayH1}>Handler</button>
+        </>
+    )
+
+}
+export default UseRef;
