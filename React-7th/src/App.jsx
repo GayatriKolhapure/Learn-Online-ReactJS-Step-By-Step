a234a2c6e07620b9e084e2c20390b8bb38897969
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2026-06-24 18:13:57 +0530

    learned install tailwindcss in react and started API in that get method done

---
+++ React-7th/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    getUsersData();
+  }, []);
+
+  const getUsersData = async () => {
+    const url = "https://dummyjson.com/users";
+    let response = await fetch(url);
+    response = await response.json();
+
+    //console.log(response);
+    setUsersData(response.users);
+  };
+
+  // console.log(usersData);
+
+  return (
+    <>
+      {/* <h2 className='bg-red-400 border-green-500 border-2 '>Tailwind CSS setup</h2> */}
+
+      <h2 className="title">Fetched Data from Dummy API</h2>
+
+      <div className="user-container">
+        {usersData &&
+          usersData.map((user) => (
+            <div className="user-card" key={user.id}>
+              <h3>
+                {user.firstName} {user.lastName}
+              </h3>
+              <p>Age: {user.age}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
