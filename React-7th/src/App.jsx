ebd98f22ff58c66721cb176b90e357f9bbcdb0ab
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2026-06-25 17:57:13 +0530

    learned make API with JSON server and integrated JSON server API and loader

--- React-7th/src/App.jsx
+++ React-7th/src/App.jsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import JsonEx from "./JsonServerEx";
 
 function App() {
-  const [usersData, setUsersData] = useState([]);
+  // const [usersData, setUsersData] = useState([]);
 
-  useEffect(() => {
-    getUsersData();
-  }, []);
+  // useEffect(() => {
+  //   getUsersData();
+  // }, []);
 
-  const getUsersData = async () => {
-    const url = "https://dummyjson.com/users";
-    let response = await fetch(url);
-    response = await response.json();
+  // const getUsersData = async () => {
+  //   const url = "https://dummyjson.com/users";
+  //   let response = await fetch(url);
+  //   response = await response.json();
 
-    //console.log(response);
-    setUsersData(response.users);
-  };
+  //   //console.log(response);
+  //   setUsersData(response.users);
+  // };
 
   // console.log(usersData);
 
@@ -23,7 +24,7 @@ function App() {
     <>
       {/* <h2 className='bg-red-400 border-green-500 border-2 '>Tailwind CSS setup</h2> */}
 
-      <h2 className="title">Fetched Data from Dummy API</h2>
+      {/* <h2 className="title">Fetched Data from Dummy API</h2>
 
       <div className="user-container">
         {usersData &&
@@ -35,7 +36,11 @@ function App() {
               <p>Age: {user.age}</p>
             </div>
           ))}
-      </div>
+      </div> */}
+
+
+
+      <JsonEx/>
     </>
   );
 }
---
+++ React-7th/src/JsonServerEx.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+function JsonEx() {
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    const url = "http://localhost:3000/users";
+
+    let response = await fetch(url);
+    response = await response.json();
+    console.log(response);
+    setUserData(response);
+    setLoading(false);
+  };
+
+ return (
+  <div className="container">
+    <h2 className="title">Integrated JSON Server API and Loader</h2>
+
+    <div className="user-list">
+      
+      {  !loading? 
+      userData && 
+        userData.map((user) => (
+          <div className="user-card" key={user.id}>
+            <h3>{user.name}</h3>
+            <p>Email: {user.email}</p>
+            <p>Age: {user.age}</p>
+          </div>
+        )):<h1>Data Loading...</h1>}
+        
+    </div>
+  </div>
+);
+}
+
+export default JsonEx;
